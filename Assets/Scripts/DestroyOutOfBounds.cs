e55a366d65acab7059097dbb2d1e0f5494c24c29
Lukasz <lukas.gluck.04@gmail.com>
2022-12-09 10:49:09 +0100

    Added Destroy Out Of Bounds script, which destroys objects that are exiting left of the screen

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyProjectileScript : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,9 @@ public class EnemyProjectileScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -12)
+        {
+            Destroy(gameObject);
+        }
     }
-
 }
